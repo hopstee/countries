3bf792c68132c56403f421d35190e5da49f295bc
hopstee <hopsteee@gmail.com>
2021-09-17 22:58:07 +0300

    add data cashing

--- pages/api/getCountriesCodes.js
+++ pages/api/getCountriesCodes.js
@@ -2,7 +2,7 @@ import fs from "fs";
 
 export default async (req, res) => {
     const updateDataFile = process.cwd() + '/data/countries_codes.json'
-    const data = JSON.parse(fs.readFileSync(updateDataFile))
+    const data = fs.readFileSync(updateDataFile)
 
-    res.status(200).json(data)
+    res.status(200).json(JSON.parse(data))
 }
